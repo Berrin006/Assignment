17cb88dc10624266c5033ef75db9440243cb0d90
berrink <berrin.kizildagli@yh.nackademin.se>
2025-09-18 10:05:51 +0200

    IProductService and product result model created

---
+++ Infrastracture0918/Interfaces/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Infrastracture0918.Models;
+
+namespace Infrastracture0918.Interfaces;
+
+public interface IProductService
+{
+    ProductResult AddProductToList(Product product);
+
+    ProductResult<IEnumerable<Product>> GetAllProducts();
+}
